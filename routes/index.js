d2de3b8aa638fcf096512023a0ce9debbce667f4
deeseert <gio.amms@gmail.com>
2020-05-14 07:35:29 +0100

    Set up User Dashboard

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/user');
+const Campground = require('../models/campground');
 const passport = require('passport');
 
 
@@ -16,8 +17,8 @@ router.get('/register', (req, res) => {
 
 //handle sign up logic
 router.post('/register', (req, res) => {
-  const { username, password, adminCode } = req.body;
-  const newUser = new User({ username });
+  const { username, password, avatar, firstName, lastName, email, adminCode } = req.body;
+  const newUser = new User({ username, avatar, firstName, lastName, email });
 
   if (adminCode === process.env.ADMIN_CODE) {
     newUser.isAdmin = true;
@@ -56,6 +57,23 @@ router.get('/logout', (req, res) => {
   req.flash('success', 'Logged you out!');
   req.logout();
   res.redirect('/campgrounds');
+});
+
+// USER PROFILE
+router.get("/users/:id", function (req, res) {
+  User.findById(req.params.id, function (err, foundUser) {
+    if (err) {
+      req.flash("error", "Something went wrong.");
+      return res.redirect("/");
+    }
+    Campground.find().where('author.id').equals(foundUser._id).exec(function (err, campgrounds) {
+      if (err) {
+        req.flash("error", "Something went wrong.");
+        return res.redirect("/");
+      }
+      res.render("users/show", { user: foundUser, campgrounds: campgrounds });
+    })
+  });
 });
 
 module.exports = router;
